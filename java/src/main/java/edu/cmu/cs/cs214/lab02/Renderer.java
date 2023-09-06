6cc685ec2d174625ccbdbd0d6a7ef31f147b6acc
Chloe <Chloe.deng1000@gmail.com>
2023-09-06 01:37:32 -0400

    Changed Renderer class to accept Shapes

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
@@ -3,14 +3,14 @@ package edu.cmu.cs.cs214.lab02;
 import edu.cmu.cs.cs214.lab02.shapes.*;
 
 public class Renderer {
-    private Rectangle rectangle;
+    // private Rectangle rectangle;
     
-    Renderer(Rectangle rectangle) {
-        this.rectangle = rectangle;
-    }
+    // Renderer(Rectangle rectangle) {
+    //     this.rectangle = rectangle;
+    // }
 
-    void draw() {
-        double area = rectangle.getArea();
+    void draw(Shape shape) {
+        double area = shape.getArea();
 
         // assume implementation
 
