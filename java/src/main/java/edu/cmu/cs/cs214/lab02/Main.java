9d28af29c75aeaa3af49ad2eeb2f36e4e1f6b77a
Chloe <Chloe.deng1000@gmail.com>
2023-09-06 12:45:03 -0400

    Modularized code in Renderer for readability

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,14 +1,13 @@
 package edu.cmu.cs.cs214.lab02;
 
-import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
-import edu.cmu.cs.cs214.lab02.shapes.Shape;
+import edu.cmu.cs.cs214.lab02.shapes.*;
 
 public class Main {
     public static void main(String[] args) {
         Shape rectangle = new Rectangle(2,3);
 
-        Renderer renderer = new Renderer();
+        Renderer renderer = new Renderer(rectangle);
 
-        renderer.draw(rectangle);
+        renderer.draw();
     }
 }
